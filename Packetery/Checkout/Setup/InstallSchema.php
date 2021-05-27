fe8a6617f35c75355a925d51a0a7981fd1a44b87
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2021-05-27 14:04:57 +0200

    PES-182: multi detail

--- Packetery/Checkout/Setup/InstallSchema.php
+++ Packetery/Checkout/Setup/InstallSchema.php
@@ -293,20 +293,12 @@ class InstallSchema implements InstallSchemaInterface
                 'type' => Table::TYPE_INTEGER,
                 'attr' => ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
             ],
-            'carrier_id' => [
-                'type' => Table::TYPE_INTEGER,
-                'attr' => [
-                    'nullable' => false,
-                    'after' => 'id',
-                    'length' => '11',
-                ],
-            ],
             'name' => [
                 'type' => Table::TYPE_TEXT,
                 'attr' => [
                     'nullable' => false,
                     'length' => '64',
-                    'after' => 'carrier_id'
+                    'after' => 'id'
                 ]
             ],
             'is_pickup_points' => [
