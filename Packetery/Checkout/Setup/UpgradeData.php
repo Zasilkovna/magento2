d9db410a36c2750c33d5fe775f81d1248e7034a1
FJiskra <filip.jiskra@gmail.com>
2021-05-28 11:07:46 +0200

    PES-155: packeteryCarrier refactor - migration for removed global default price

--- Packetery/Checkout/Setup/UpgradeData.php
+++ Packetery/Checkout/Setup/UpgradeData.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace Packetery\Checkout\Setup;
 
-use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
 use Packetery\Checkout\Console\Command\MigratePriceRules;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
@@ -15,6 +14,9 @@ class UpgradeData implements UpgradeDataInterface
     /** @var MigratePriceRules */
     private $migratePriceRulesCommand;
 
+    /** @var \Packetery\Checkout\Console\Command\MigrateDefaultPrice */
+    private $migrateDefaultPriceCommand;
+
     /** @var \Magento\Config\Model\Config\Factory */
     private $configFactory;
 
@@ -23,11 +25,16 @@ class UpgradeData implements UpgradeDataInterface
      *
      * @param \Packetery\Checkout\Console\Command\MigratePriceRules $migratePriceRulesCommand
      * @param \Magento\Config\Model\Config\Factory $configFactory
+     * @param \Packetery\Checkout\Console\Command\MigrateDefaultPrice $migrateDefaultPriceCommand
      */
-    public function __construct(MigratePriceRules $migratePriceRulesCommand, \Magento\Config\Model\Config\Factory $configFactory)
-    {
+    public function __construct(
+        MigratePriceRules $migratePriceRulesCommand,
+        \Magento\Config\Model\Config\Factory $configFactory,
+        \Packetery\Checkout\Console\Command\MigrateDefaultPrice $migrateDefaultPriceCommand
+    ) {
         $this->migratePriceRulesCommand = $migratePriceRulesCommand;
         $this->configFactory = $configFactory;
+        $this->migrateDefaultPriceCommand = $migrateDefaultPriceCommand;
     }
 
     /**
@@ -42,6 +49,8 @@ class UpgradeData implements UpgradeDataInterface
         }
 
         if (version_compare($context->getVersion(), "2.0.6", "<")) {
+            $this->migrateDefaultPriceCommand->run(new ArrayInput([]), new NullOutput());
+
             $configModel = $this->configFactory->create();
             $configModel->setDataByPath('carriers/packetery/sallowspecific', 0); // config option UI was removed
             $configModel->save();
