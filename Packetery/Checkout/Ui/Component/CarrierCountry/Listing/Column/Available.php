f657a3e1c7185a9afe4880bdebb8458c79f07f10
FJiskra <filip.jiskra@gmail.com>
2021-06-10 16:29:10 +0200

    PES-182: pricing rules multi detail - available filtering

--- Packetery/Checkout/Ui/Component/CarrierCountry/Listing/Column/Available.php
+++ Packetery/Checkout/Ui/Component/CarrierCountry/Listing/Column/Available.php
@@ -14,16 +14,12 @@ class Available extends Column
     /** @var \Magento\Directory\Model\CountryFactory */
     protected $countryFactory;
 
-    /** @var \Packetery\Checkout\Ui\Component\CarrierCountry\Form\Modifier */
-    private $modifier;
-
     /**
      * Country constructor.
      *
      * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
      * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
      * @param \Magento\Directory\Model\CountryFactory $countryFactory
-     * @param \Packetery\Checkout\Ui\Component\CarrierCountry\Form\Modifier $modifier
      * @param array $components
      * @param array $data
      */
@@ -31,13 +27,11 @@ class Available extends Column
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         CountryFactory $countryFactory,
-        \Packetery\Checkout\Ui\Component\CarrierCountry\Form\Modifier $modifier,
         array $components = [],
         array $data = []
     ) {
         parent::__construct($context, $uiComponentFactory, $components, $data);
         $this->countryFactory = $countryFactory;
-        $this->modifier = $modifier;
     }
 
     /**
@@ -47,14 +41,9 @@ class Available extends Column
     public function prepareDataSource(array $dataSource): array {
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as &$item) {
-
-                $options = $this->modifier->getCarriers($item['country']);
-
-                if (!empty($options)) {
-                    $item['available'] = '1';
+                if ($item['available'] === '1') {
                     $item[$this->getData('name')] = __('Yes');
                 } else {
-                    $item['available'] = '0';
                     $item[$this->getData('name')] = __('No');
                 }
             }
--- Packetery/Checkout/Ui/Component/CarrierCountry/Listing/SearchResult.php
+++ Packetery/Checkout/Ui/Component/CarrierCountry/Listing/SearchResult.php
@@ -4,8 +4,58 @@ declare(strict_types=1);
 
 namespace Packetery\Checkout\Ui\Component\CarrierCountry\Listing;
 
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
 class SearchResult extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
 {
+    /** @var \Packetery\Checkout\Ui\Component\CarrierCountry\Form\Modifier */
+    private $modifier;
+
+    /** @var \Magento\Framework\App\RequestInterface */
+    private $request;
+
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        \Packetery\Checkout\Ui\Component\CarrierCountry\Form\Modifier $modifier,
+        \Magento\Framework\App\RequestInterface $request,
+        $mainTable,
+        $resourceModel = null,
+        $identifierName = null,
+        $connectionName = null
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel, $identifierName, $connectionName);
+        $this->modifier = $modifier;
+        $this->request = $request;
+    }
+
+    /**
+     * @param string|array $field
+     * @param null|string|array $condition
+     * @return $this
+     */
+    public function addFieldToFilter($field, $condition = null) {
+        if ($field === 'availableName') {
+            return $this;
+        }
+
+        if (is_array($field) && in_array('availableName', $field)) {
+            unset($field[array_search('availableName', $field)]);
+            unset($condition[array_search('availableName', $field)]);
+
+            if (empty($field)) {
+                return $this;
+            }
+        }
+
+        return parent::addFieldToFilter($field, $condition);
+    }
+
     protected function _initSelect() {
         $packeteryCarrierTable = $this->getTable('packetery_carrier');
         $this->getSelect()
@@ -20,6 +70,33 @@ class SearchResult extends \Magento\Framework\View\Element\UiComponent\DataProvi
                     'country',
                 ]
             );
+
+        return $this;
+    }
+
+    protected function _afterLoadData() {
+        parent::_afterLoadData();
+
+        $filters = $this->request->getParam('filters', []);
+
+        foreach ($this->_data as $key => &$item) {
+
+            $options = $this->modifier->getCarriers($item['country']);
+
+            if (!empty($options)) {
+                $item['available'] = '1';
+            } else {
+                $item['available'] = '0';
+            }
+
+            if (isset($filters['availableName'])) {
+                $value = $filters['availableName'];
+
+                if ($item['available'] !== $value) {
+                    unset($this->_data[$key]);
+                }
+            }
+        }
 
         return $this;
     }
