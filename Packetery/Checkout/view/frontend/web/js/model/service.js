81ffebd23300f8b2d61edaf0b35bf011dfd5223a
FJiskra <filip.jiskra@gmail.com>
2021-11-25 09:50:17 +0100

    PES-414: widget HD - security issue TODO comment

--- Packetery/Checkout/view/frontend/web/js/model/service.js
+++ Packetery/Checkout/view/frontend/web/js/model/service.js
@@ -4,7 +4,7 @@ define([], function () {
     return {
         getPacketaPoint: function(defaultReturnValue) {
             if (localStorage.packetaPoint) {
-                return JSON.parse(localStorage.packetaPoint);
+                return JSON.parse(localStorage.packetaPoint); // TODO: Store data in backend to avoid direct edit by unwanted actor. Issue was communicated with manager.
             }
 
             return defaultReturnValue;
@@ -12,7 +12,7 @@ define([], function () {
 
         getPacketaValidatedAddress: function(defaultReturnValue) {
             if (localStorage.packetaValidatedAddress) {
-                return JSON.parse(localStorage.packetaValidatedAddress);
+                return JSON.parse(localStorage.packetaValidatedAddress); // TODO: Store data in backend to avoid direct edit by unwanted actor. Issue was communicated with manager.
             }
 
             return defaultReturnValue;
