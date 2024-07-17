8a71d07536f2537589348c8cf8d0d1f2b96ec9b2
David Belka <david.belka@yahoo.co.uk>
2024-07-17 16:32:21 +0200

    PES-2180: Proof of Concept.

---
+++ Packetery/Checkout/Block/Adminhtml/Product/Edit/AddModalButton.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Packetery\Checkout\Block\Adminhtml\Product\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class AddModalButton implements ButtonProviderInterface
+{
+    public function getButtonData(): array
+    {
+        return [
+            'label' => __('View'),
+            'class' => 'action-secondary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'Magento_Ui/js/form/button-adapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'product_form.product_form.test_modal',
+                                'actionName' => 'openModal'
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'on_click' => '',
+            'sort_order' => 10
+        ];
+    }
+}
