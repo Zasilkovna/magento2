08d414bed8d2dd2cac9b53cfd45d1c9844a7e019
David Belka <david.belka@yahoo.co.uk>
2024-07-19 10:51:28 +0200

    PES-2180: WIP

--- Packetery/Checkout/Ui/Component/Log/Listing/Column/Actions.php
+++ Packetery/Checkout/Ui/Component/Log/Listing/Column/Actions.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Packetery\Checkout\Ui\Component\Log\Listing\Column;
 
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
 
 class Actions extends Column
@@ -13,26 +16,28 @@ class Actions extends Column
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as &$item) {
                 $name = $this->getData('name');
-                $item[$name]['view'] = [
-                    'label' => __('View'),
-                    'data_attribute' => [
-                        'mage-init' => [
-                            'Magento_Ui/js/form/button-adapter' => [
-                                'actions' => [
-                                    [
-                                        'targetName' => 'packetery_log_modal.packetery_log_modal.test_modal',
-                                        'actionName' => 'openModal'
-                                    ]
-                                ]
-                            ]
-                        ]
+                $item[$name]['edit'] = [
+                    'callback' => [
+                        [
+                            'provider' => 'packetery_checkout_log_listing.test_modal',
+                            'target' => 'openModal',
+                        ],
+//                        [
+//                            'provider' => 'customer_form.areas.address.address'
+//                                . '.customer_address_update_modal.update_customer_address_form_loader',
+//                            'target' => 'render',
+//                            'params' => [
+//                                'entity_id' => $item['entity_id'],
+//                            ],
+//                        ]
                     ],
-                    'on_click' => 'console.log(1)',
-                    'sort_order' => 10
+                    'href' => '#',
+                    'label' => __('Edit'),
+                    'hidden' => false,
                 ];
             }
         }
+
         return $dataSource;
     }
-
 }
