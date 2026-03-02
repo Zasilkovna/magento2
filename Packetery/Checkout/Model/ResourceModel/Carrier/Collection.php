07cf8c8120edc617b029761445a92224f1d7ef5e
J7F <J7F@gmail.com>
2026-03-02 12:01:29 +0100

    PES-3099: allow carriers requiring customs declaration

--- Packetery/Checkout/Model/ResourceModel/Carrier/Collection.php
+++ Packetery/Checkout/Model/ResourceModel/Carrier/Collection.php
@@ -64,7 +64,6 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
     private function supportedOnly(): void {
         $this->addFieldToFilter('main_table.carrier_id', ['nin' => [257, 136, 134, 132]]); // večerní doručení todo implement ZIP code logic
         $this->addFilter('main_table.disallows_cod', 0); // todo implement payment method filter
-        $this->addFilter('main_table.customs_declarations', 0); // todo what does it require? New order edit form fields?
     }
 
     /**
